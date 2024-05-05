e2ba957a3835ed8f50f99465172c801f1b859e42
IRTIDEATH <enderjhass@gmail.com>
2024-05-05 17:06:08 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center w-full min-h-screen">
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default App
