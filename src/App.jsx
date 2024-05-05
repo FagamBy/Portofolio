9b0179a1271936c774c8d5030c024cb7f43ed142
IRTIDEATH <enderjhass@gmail.com>
2024-05-05 17:10:30 +0700

    upd: text #1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <div className="flex items-center justify-center w-full min-h-screen">
-      <h1>hello</h1>
+      <h1>Web ini sedang dalam pembuatan</h1>
+      <p>(Sedang membuat desain)</p>
     </div>
   )
 }
