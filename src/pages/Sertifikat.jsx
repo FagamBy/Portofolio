4b6fadee96be279075fe24756ac62c24ba1e0c93
IRTIDEATH <enderjhass@gmail.com>
2024-06-05 18:14:38 +0700

    upd: add carousel in sertificate section

--- src/pages/Sertifikat.jsx
+++ src/pages/Sertifikat.jsx
@@ -1,15 +1,31 @@
-import React from "react";
-import {motion} from 'framer-motion'
+import * as React from "react";
+import { motion } from "framer-motion";
+
+import Autoplay from "embla-carousel-autoplay"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card"
 
 const Sertifikat = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 3000 })
+  );
   return (
     <div className="bg-[#292e39] flex flex-col">
       <div className="spacer layer3 absolute"></div>
 
-      <div id="project" className="min-h-[100vh] w-full z-10 pt-16 md:mt-40 sm:mt-28 xs:mt-16 lg:mt-48 xl:mt-60">
+      <div
+        id="project"
+        className="min-h-[100vh] w-full z-10 pt-16 md:mt-40 sm:mt-28 xs:mt-16 lg:mt-48 xl:mt-60"
+      >
         <div className="flex flex-col justify-center">
           <div className="flex flex-col items-center space-y-3">
-            <h1 className="text-3xl text-[#ECEFF4] font-medium">My Project</h1>
+            <h1 className="text-3xl text-[#ECEFF4] font-medium">My Sertificate</h1>
             <motion.div
               className="w-[110px] bg-[#808691] py-[1px]"
               initial={{ width: "0px" }}
@@ -17,6 +33,33 @@ const Sertifikat = () => {
               viewport={{ once: true }}
               transition={{ duration: 0.7 }}
             ></motion.div>
+          </div>
+
+          <div className="w-full flex justify-center mt-5">
+            <Carousel
+              plugins={[plugin.current]}
+              className="w-full max-w-xs"
+              onMouseEnter={plugin.current.stop}
+              onMouseLeave={plugin.current.play}
+            >
+              <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <CarouselItem key={index}>
+                    <div className="p-1">
+                      <Card>
+                        <CardContent className="flex aspect-square items-center justify-center p-6">
+                          <span className="text-4xl font-semibold">
+                            {index + 1}
+                          </span>
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
           </div>
         </div>
       </div>
