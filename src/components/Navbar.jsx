0f66a8837ab72531fb5451d533fbecdb5b213968
IRTIDEATH <enderjhass@gmail.com>
2024-06-07 21:43:59 +0700

    upd: add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import { navLinks } from "../constants";
 import { close, menu } from "../assets";
 
-
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const [scroll, setScroll] = useState(false);
@@ -13,13 +12,43 @@ const Navbar = () => {
     });
   }, []);
   return (
-    <nav className={`w-full flex py-5 justify-between items-center top-0 right-0 px-8 big:px-20 xl:px-36 xxl:px-40 z-[1000]
+    <nav
+      className={`w-full flex py-5 justify-between items-center top-0 right-0 px-8 big:px-20 xl:px-36 xxl:px-40 z-[1000]
       fixed ${scroll ? "stickys" : "bstickys"}
       bg-[#2e3440]
       `}
       // lanjutin benerin mx dan px nya di ukuran layar besar
     >
-      <h1 className="text-[#ECEFF4] font-semibold text-2xl">Mond</h1>
+      <div className="flex items-center space-x-2">
+        <svg
+          className="w-[40px] h-[40px] object-contain"
+          width="135"
+          height="148"
+          viewBox="0 0 135 148"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M2 2H22.6415L113.827 93.1852L108.919 98.0933L19.7664 8.94118H8.94118V122.047H85.3594V128.989H2V2Z"
+            fill="#ECEFF4"
+            stroke="#ECEFF4"
+            stroke-width="3"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M132.982 144L82.0936 92.9987L87.0072 88.096L126.041 127.216V9.17255H52.8034V2.23137H132.982V144Z"
+            fill="#ECEFF4"
+            stroke="#ECEFF4"
+            stroke-width="3"
+          />
+        </svg>
+
+        <h1 className={`text-[#ECEFF4] font-semibold text-3xl ${scroll ? "hidden" : "flex"}`}>Mond</h1>
+        {/* tammbahin animation sebelum scroll dan sesudah scroll */}
+      </div>
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
@@ -63,7 +92,6 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-
     </nav>
   );
 };
