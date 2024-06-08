c37e3101b9644c09d7c547437d24d1ea05ec5d1e
IRTIDEATH <enderjhass@gmail.com>
2024-06-08 14:18:34 +0700

    upd: add accordion and new text in about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import { Backpack } from "../assets/icon/Backpack";
 import { motion } from "framer-motion";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 const About = () => {
   return (
     <div className="bg-[#292e39] flex flex-col">
       <div className="spacer layer1 flex items-center justify-start px-8 big:px-20 xl:px-36 xxl:px-40 absolute">
-        <Backpack/>
+        <Backpack />
       </div>
 
-      <div id="about"
+      <div
+        id="about"
         className="w-full pt-16
         z-10 md:mt-40 sm:mt-32 xs:mt-16 lg:mt-48 xl:mt-60"
       >
@@ -17,16 +25,47 @@ const About = () => {
           <div className="flex flex-col items-center space-y-3">
             <h1 className="text-3xl text-[#ECEFF4] font-medium">About me</h1>
             <motion.div
-                className="w-[115px] bg-[#808691] py-[1px]"
-                initial={{ width: "0px" }}
-                whileInView={{ width: "115px" }}
-                viewport={{once: true}}
-                transition={{ duration: 0.7, delay: 0.2 }}
+              className="w-[115px] bg-[#808691] py-[1px]"
+              initial={{ width: "0px" }}
+              whileInView={{ width: "115px" }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.7, delay: 0.2 }}
             ></motion.div>
           </div>
 
-          <div className="flex ss:flex-row flex-col ss:space-y-0 space-y-9 items-center justify-between bg-[#434C5E] mx-8 big:mx-20 xl:mx-36 xxl:mx-40 p-5 mt-8 rounded-md">
-            <div className="flex flex-col ss:w-[40%] space-y-3">
+          <div className="flex items-center justify-center mx-8 big:mx-20 xl:mx-36 xxl:mx-40 p-5 mt-8 rounded-md">
+            <div className="w-[40%]">
+              <Accordion type="single" collapsible className="w-full">
+                <AccordionItem value="item-2">
+                  <AccordionTrigger>Data</AccordionTrigger>
+                  <AccordionContent>
+                    <ul className="text-[#E5E9F0] text-sm space-y-2">
+                      <li>Name : Al Sakha Fatthariq Anuar</li>
+                      <li>Status : Student</li>
+                      <li>Age : 17</li>
+                      <li>City : Jakarta, Indonesia</li>
+                      <li>Hobby : Reading novel, Gaming, Coding</li>
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>My Story</AccordionTrigger>
+                  <AccordionContent>
+                    <div className="space-y-3">
+                      <p className="text-justify text-wrap text-[#D8DEE9] text-sm">
+                        Since I was little, I couldn't do mathematics. When I
+                        graduated from junior high school I entered the software
+                        engineering department.
+                        I knew it would require very good math skills there.
+                        But it's okay, I'll try my best
+                      </p>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </div>
+
+            {/* <div className="flex flex-col ss:w-[40%] space-y-3">
               <h1 className="text-[#eceff4]">- My Story</h1>
               <p className="text-justify text-wrap text-[#D8DEE9] text-sm">
                 The beginning of learning basic programming was very fun, but
@@ -43,15 +82,15 @@ const About = () => {
                 my goal in learning programming. And I decided
                 to learn programming more often until I master it
               </p>
-            </div>
+            </div> */}
 
-            <div className="bg-[#3b4252] ss:w-[40%] flex p-5 rounded-md">
+            {/* <div className="bg-[#3b4252] ss:w-[40%] flex p-5 rounded-md">
               <ul className="text-[#E5E9F0] text-lg">
                 <li>Hobby : Reading novel, Gaming, Coding</li>
                 <li>Status : Student</li>
                 <li>Age : 17</li>
               </ul>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
