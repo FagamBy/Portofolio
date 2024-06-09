7bb5602f5472794d2e654dbf535c49115ed0324a
IRTIDEATH <enderjhass@gmail.com>
2024-06-09 13:28:27 +0700

    upd: add a card

---
+++ src/components/Card.jsx
@@ -0,0 +1,85 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import pancarimaimg from "/pancarimaimg.png";
+import nexcentimg from "/nexcentimg.png";
+import kasirappimg from "/kasirappimg.png"
+import inditechimg from "/inditechimg.png"
+
+export const Card1 = () => {
+  return (
+    <div className="bg-[#4C566A] text-[#88C0D0] flex flex-col rounded-b-md">
+      <img src={nexcentimg} alt="" />
+      <div className="p-5 flex flex-col space-y-3">
+        <h1 className="text-2xl font-semibold">Nexcent</h1>
+        <div className="flex space-x-4 text-xl">
+          <button>
+            <FontAwesomeIcon icon={faGithub} />
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faPlay} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const Card2 = () => {
+  return (
+    <div className="bg-[#4C566A] text-[#88C0D0] flex flex-col rounded-b-md">
+      <img src={pancarimaimg} alt="" />
+      <div className="p-5 flex flex-col space-y-3">
+        <h1 className="text-2xl font-semibold">Pancarima</h1>
+        <div className="flex space-x-4 text-xl">
+          <button>
+            <FontAwesomeIcon icon={faGithub} />
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faPlay} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const Card3 = () => {
+  return (
+    <div className="bg-[#4C566A] text-[#88C0D0] flex flex-col rounded-b-md">
+      <img src={kasirappimg} alt="" />
+      <div className="p-5 flex flex-col space-y-3">
+        <h1 className="text-2xl font-semibold">Kasir Desktop</h1>
+        <div className="flex space-x-4 text-xl">
+          <button>
+            <FontAwesomeIcon icon={faGithub} />
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faPlay} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export const Card4 = () => {
+    return (
+      <div className="bg-[#4C566A] text-[#88C0D0] flex flex-col rounded-b-md">
+        <img src={inditechimg} alt="" />
+        <div className="p-5 flex flex-col space-y-3">
+          <h1 className="text-2xl font-semibold">Inditech</h1>
+          <div className="flex space-x-4 text-xl">
+            <button>
+              <FontAwesomeIcon icon={faGithub} />
+            </button>
+            <button>
+              <FontAwesomeIcon icon={faPlay} />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {motion} from 'framer-motion'
+import { Card1, Card2, Card3, Card4 } from "@/components/Card";
 
 const Project = () => {
   return (
     <div className="bg-[#2e3440] flex flex-col">
       <div className="spacer layer4 absolute"></div>
 
-      <div id="project" className="w-full min-h-[100vh] z-10 pt-16 md:mt-40 sm:mt-28 xs:mt-16 lg:mt-48 xl:mt-60">
+      <div id="project" className="w-full min-h-[150vh] z-10 pt-16 md:mt-40 sm:mt-28 xs:mt-16 lg:mt-48 xl:mt-60">
         <div className="flex flex-col justify-center">
           <div className="flex flex-col items-center space-y-3">
             <h1 className="text-3xl text-[#ECEFF4] font-medium">My Projects</h1>
@@ -17,6 +18,15 @@ const Project = () => {
               viewport={{ once: true }}
               transition={{ duration: 0.7, delay: 0.2 }}
             ></motion.div>
+          </div>
+
+          <div className="mx-8 big:mx-20 xl:mx-36 xxl:mx-40 mt-16">
+            <div className="grid grid-cols-3 gap-6">
+              <Card1/>
+              <Card2/>
+              <Card3/>
+              <Card4/>
+            </div>
           </div>
         </div>
       </div>
