88154b4e38e32711e6b866e9fdbfb4d83d4a0fe1
IRTIDEATH <enderjhass@gmail.com>
2024-08-25 21:36:00 +0700

    upd: add more project in project section

--- src/components/CardP.jsx
+++ src/components/CardP.jsx
@@ -17,6 +17,7 @@ import inditechimg from "/inditechimg.png";
 import irtitodo from "/irtitodo.png";
 import dhafaart from "/dhafaart.png";
 import zabod from "/zabod.png";
+import zentra from "/zentra.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -63,7 +64,7 @@ export default function CardP() {
         <CardContent className="p-4">
           <LazyLoadImage src={pancarimaimg} alt="" className="rounded-md mb-4" />
           <p className="text-[#D8DEE9]">
-            Ini adalah website dari kegiatan lomba Sagasitas AWS 2024.
+            Ini adalah website dari kegiatan lomba Sagasitas February 2024.
           </p>
         </CardContent>
         <CardFooter className="p-4">
@@ -192,6 +193,35 @@ export default function CardP() {
             </a>
             <a
               href="https://zabod.vercel.app/"
+              className="hover:text-[#88C0D0] hover:bg-transparent border-2 border-[#88C0D0] hover:border-2 transition duration-200 bg-[#88C0D0] p-[0.4rem] rounded-sm"
+            >
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} /> Demo
+            </a>
+          </div>
+        </CardFooter>
+      </Card>
+      <Card className="rounded-lg bg-[#292e39] text-[#2e3440]">
+        <CardHeader className="p-4">
+          <CardTitle className="text-xl font-semibold text-[#88C0D0]">
+            ZentraHealth
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="p-4">
+          <LazyLoadImage src={zentra} alt="" className="rounded-md mb-4" />
+          <p className="text-[#D8DEE9]">
+            Ini adalah website dari kegiatan lomba Sagasitas Agustus 2024.
+          </p>
+        </CardContent>
+        <CardFooter className="p-4">
+          <div className="flex items-center space-x-4 text-sm font-medium">
+            <a
+              aria-disabled
+              className="hover:text-[#88C0D0] hover:bg-transparent border-2 border-[#88C0D0] hover:border-2 transition duration-200 bg-[#88C0D0] p-[0.4rem] rounded-sm cursor-not-allowed"
+            >
+              <FontAwesomeIcon className="text-[1rem]" icon={faGithub} /> Github
+            </a>
+            <a
+              href="https://zentrahealth.vercel.app/"
               className="hover:text-[#88C0D0] hover:bg-transparent border-2 border-[#88C0D0] hover:border-2 transition duration-200 bg-[#88C0D0] p-[0.4rem] rounded-sm"
             >
               <FontAwesomeIcon icon={faArrowUpRightFromSquare} /> Demo
